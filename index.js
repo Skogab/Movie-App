f20e9f32131c801449e835df3ec10e12a6b7ee14
Skogab <114601288+Skogab@users.noreply.github.com>
2023-02-20 11:40:35 +0100

    2.5 finished Update Index.js and Documentation Endpoitns
    - fixed index.js for Director
    . Updated Documentation with endpoints

--- index.js
+++ index.js
@@ -228,16 +228,16 @@ app.get("/movies/genre/:genreName", (req, res) => {
 // Get get movie by director
 app.get("/movies/directors/:directorName", (req, res) => {
   const { directorName } = req.params;
-  const directorMovies = movies.filter(
-    (movie) => movie.director.name === directorName
-  );
+  const directorMovies = movies.filter((movie) => movie.director.name === directorName);
 
   if (directorMovies.length > 0) {
     res.status(200).json(directorMovies);
   } else {
-    res.status(400).send("No such Director in Database");
+    res.status(400).send("No such director in database");
   }
 });
+
+
 
 // app.listen console.log
 app.listen(8080, () => {
