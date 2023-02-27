a8d2ea3834c53ad8004330d54a1d5e0ed312e6ec
Skogab <114601288+Skogab@users.noreply.github.com>
2023-02-27 19:04:20 +0100

    2.10 Heroku Setup
    - Express Validator
    - hashed passwords

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
+const { check, validationResult } = require('express-validator');
+
 mongoose.set("strictQuery", true);
 
 const fs = require("fs");
@@ -19,32 +21,38 @@ const express = require("express");
   (bodyParser = require("body-parser")),
   (uuid = require("uuid"));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 app.use(express.static("public"));
 app.use(morgan("common"));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+let auth = require('./auth')(app);
+const cors = require('cors');
+app.use(cors());
 
 
 
 
+
+
+
+/* rest of code goes here*/
 
 
 
 // Get all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find()
     .then((movies) => {
-      res.status(201).json(movies);
+      res.status(200).json(movies);
     })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     });
-});
+}); 
 
 
 // Get all users
@@ -75,33 +83,44 @@ app.get('/users/:Username', (req, res) => {
 
 
 
-//Add a user JSON
-/* JSON
-{
-  ID: Integer,
-  Username: String,
-  Password: String,
-  Email: String,
-  Birthday: Date
-}*/
-app.post('/users', (req, res) => {
-  Users.findOne({ Username: req.body.Username })
+//Add a user
+
+app.post('/users',
+  // Validation logic here for request
+  //you can either use a chain of methods like .not().isEmpty() ???
+  //which means "opposite of isEmpty" in plain english "is not empty" ???
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
+
+  // check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
     .then((user) => {
       if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
+      //If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + ' already exists');
       } else {
         Users
           .create({
             Username: req.body.Username,
-            Password: req.body.Password,
+            Password: hashedPassword, // hashed password
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
-          .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
       }
     })
     .catch((error) => {
@@ -116,11 +135,12 @@ app.post('/users', (req, res) => {
 {
   ID: Integer,
   Tile: String,
+  Description: String,
   Genre Name: String,
   Director Name: String,
   Director Birth: String
 }*/
-app.post('/movies', (req, res) => {
+/* app.post('/movies', (req, res) => {
   Movies.findOne({ Title: req.body.Title })
     .then((movie) => {
       if (movie) {
@@ -129,8 +149,10 @@ app.post('/movies', (req, res) => {
         Movies
         .create({
           Title: req.body.Title,
+          Description: req.body.Description,
           Genre: {
-            Name: req.body.GenreName
+            Name: req.body.GenreName,
+            Description: req.body.GenreDescription,
           },
           Director: {
             Name: req.body.DirectorName,
@@ -148,12 +170,28 @@ app.post('/movies', (req, res) => {
       console.error(error);
       res.status(500).send('Error: ' + error);
     });
-});
+}); */
 
 
 
 // Add a movie to a user's list of favorites
-app.post('/users/:Username/movies/:MovieID', (req, res) => {
+app.post('/users/:Username/movies/:MovieID',
+  // Validation logic here for request
+  //you can either use a chain of methods like .not().isEmpty() ???
+  //which means "opposite of isEmpty" in plain english "is not empty" ???
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
+
+  // check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
   Users.findOneAndUpdate({ Username: req.params.Username }, {
        $push: { FavoriteMovies: req.params.MovieID }
      },
@@ -169,15 +207,24 @@ app.post('/users/:Username/movies/:MovieID', (req, res) => {
   });  
 
 
-// Update a user's info, by username JSON
-/* 
-{
-  Username: String, (required)
-  Password: String, (required)
-  Email: String,(required)
-  Birthday: Date
-}*/
-app.put('/users/:Username', (req, res) => {
+// Update a user's info
+app.put('/users/:Username',
+  // Validation logic here for request
+  //you can either use a chain of methods like .not().isEmpty()
+  //which means "opposite of isEmpty" in plain english "is not empty
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
+
+  // check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
   Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
     {
       Username: req.body.Username,
@@ -219,7 +266,8 @@ app.delete('/users/:Username', (req, res) => {
 
 
 
-// app.listen console.log
-app.listen(8080, () => {
-  console.log("Your app is listening on Port 8080.");
+// listen
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
