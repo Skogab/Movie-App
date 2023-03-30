ff8312eb047e9a3ca1d63c084eb477ed1a3ff7e4
Skogab <114601288+Skogab@users.noreply.github.com>
2023-03-30 11:42:59 +0200

    2.10 fix 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,6 @@ module.exports = {
 		"browser": true,
 		"es2021": true,
 		"node": true
-	},
-	"extends": "eslint:recommended",
-	"overrides": [
-	],
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
-	},
+	}
 	
 };
--- models.js
+++ models.js
@@ -1,12 +1,9 @@
-
-
 const bcrypt = require('bcrypt');
-
 const mongoose = require('mongoose');
 
-let movieSchema = mongoose.Schema({
-  Title: {type: String, required: true},
-  Description: {type: String, required: true},
+const movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
   Genre: {
     Name: String,
     Description: String
@@ -19,29 +16,25 @@ let movieSchema = mongoose.Schema({
   ImagePath: String,
   Featured: Boolean
 });
-  
-let userSchema = mongoose.Schema({
-    Username: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required: true},
-    Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-  });
 
+const userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
 
-
-
- userSchema.statics.hashPassword = (password) => {
+userSchema.statics.hashPassword = function (password) {
   return bcrypt.hashSync(password, 10);
 };
-// don't use arrow functions when defining methods. Eg. of Instance method that can be called on each object/document created (each individual object/document)
-userSchema.methods.validatePassword = function(password) {
+
+userSchema.methods.validatePassword = function (password) {
   return bcrypt.compareSync(password, this.Password);
 };
 
-  
-  let Movie = mongoose.model('Movie', movieSchema);
-  let User = mongoose.model('User', userSchema);
-  
-  module.exports.Movie = Movie;
-  module.exports.User = User;
+const Movie = mongoose.model('Movie', movieSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
